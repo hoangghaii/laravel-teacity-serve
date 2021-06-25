000dbf95c400a5a40907bd9bfe13fb5e7a9ac909
hoangghaii <nguyenhoanghai861@gmail.com>
2021-06-25 22:50:39 +0700

    a

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Product;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\model\Product;
+use App\Model\Product;
 use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
