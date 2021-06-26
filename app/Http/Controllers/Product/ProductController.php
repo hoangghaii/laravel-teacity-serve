ff024ef38c846d286982b91cdae86a5262009135
hoangghaii <nguyenhoanghai861@gmail.com>
2021-06-26 10:26:33 +0700

    n

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -24,7 +24,7 @@ class ProductController extends Controller
         }
         $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
         $resume = time() . '.' .  $request->file('image')->getClientOriginalExtension();
-        $request->file('image')->move(public_path()  . '/storage/public/', $resume);
+        $request->file('image')->move(public_path($actual_link)  . '/storage/', $resume);
         $product = new Product($request->all());
         $product->image = $resume;
         $product->save();
@@ -35,7 +35,7 @@ class ProductController extends Controller
         $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
         $listProduct =  Product::all();
         foreach ($listProduct as $key) {
-            $key['image'] = public_path() . '/storage/public/' . $key['image'];
+            $key['image'] = public_path($actual_link) . '/storage/' . $key['image'];
         }
         return $listProduct;
     }
