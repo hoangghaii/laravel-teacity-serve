faab1aa5797b5e7288ce4b103e7cbaebf86abec5
hoangghaii <nguyenhoanghai861@gmail.com>
2021-06-27 16:22:58 +0700

    a

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -42,19 +42,19 @@ class ProductController extends Controller
 
 
         // $resume = $request->file('image');
-        try {
-            $s3Client->putObject([
-                'Bucket' => 'teacity-storage-image',
-                'Key' =>  $resume,
-            ]);
-        } catch (S3Exception $e) {
-            // Catch an S3 specific exception.
-            echo $e->getMessage();
-        }
+        // try {
+        //     $s3Client->putObject([
+        //         'Bucket' => 'teacity-storage-image',
+        //         'Key' =>  $resume,
+        //     ]);
+        // } catch (S3Exception $e) {
+        //     // Catch an S3 specific exception.
+        //     echo $e->getMessage();
+        // }
 
-        $product = new Product($request->all());
-        $product->image = $resume;
-        $product->save();
+        // $product = new Product($request->all());
+        // $product->image = $resume;
+        // $product->save();
         // return response()->json($product);
         return $request->image;
     }
