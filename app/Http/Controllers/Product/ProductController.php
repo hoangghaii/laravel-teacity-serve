ecbcefcf027bcf1404852e929a75a02e745d30ef
hoangghaii <nguyenhoanghai861@gmail.com>
2021-06-27 17:30:51 +0700

    5

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -58,7 +58,7 @@ class ProductController extends Controller
         // return response()->json($product);
         $base64_str = substr($request->image, strpos($request->image, ",") + 1);
         // return base64_decode($base64_str);
-        return $request->file('image')->getClientOriginalExtension();
+        return $request->file('image')->get();
     }
 
     public function index()
